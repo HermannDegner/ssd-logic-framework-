c2151afd1c113160cd50e7f8455bb9bd6224ceca
Hermann Degner <Hermann_Degner@users.noreply.github.com>
2025-09-23 19:46:45 +0900

    Create index.ts

---
+++ src/human-module/index.ts
@@ -0,0 +1,9 @@
+export * from "./types/human-layers";
+export * from "./types/reaction-process";
+export * from "./types/conflicts";
+export * from "./types/exploration";
+
+export * from "./inference/human-analyzer";
+export * from "./inference/behavior-predictor";
+export * from "./inference/conflict-diagnoser";
+export * from "./inference/growth-identifier";
